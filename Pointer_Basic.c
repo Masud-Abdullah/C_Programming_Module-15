79611bbed5fcbc5f816b416ba1f1c4e33145bd96
Masud Abdullah <abdullahtamim885@gmail.com>
2023-05-03 00:29:04 +0600

    Pointer er ABCD

---
+++ Pointer_Basic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int x = 5;
+    printf("%d\n", x); // x er value
+
+    printf("%p\n", &x); // x er address
+
+    int *p = &x; // pointer variable p er moddhe x er address store korlam.
+
+    printf("%p\n", p); // etao x er address.
+
+    /// De-Reference:
+
+    printf("%d\n", *p); // de-reference er maddhome x er value print korlam.
+
+    *p = 500;
+    printf("%d\n", x); // 500 print hobe....karon *p == x. tai *p er moddhe change korle x e moddheu change hoye jabe karon tader adress same
+
+    x = 200;
+    printf("%d\n", *p); //*p r x er adress same tai x a change korleu 8p te change hoye jbe
+
+    return 0;
+}
